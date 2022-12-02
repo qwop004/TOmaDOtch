9d502654b7959addedc821db1627ecbdf2fb7a5e
YoonTaemin0044 <69154017+YoonTaemin0044@users.noreply.github.com>
2022-12-02 14:33:21 +0900

    백업

--- dama/src/dama/ToDoList.java
+++ dama/src/dama/ToDoList.java
@@ -1,8 +1,8 @@
 package dama;
-
 import java.io.*;
 import java.util.ArrayList;
-
+import java.util.Arrays;
+import java.util.List;
 
 public class ToDoList {
 	public static String filePath = "data.txt";
@@ -53,6 +53,7 @@ public class ToDoList {
         	e.printStackTrace();
         }
      }
+	
 
 }
 
